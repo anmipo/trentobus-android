7268e00d80d00415ef483e4d5917b69abf193148
Andrei Popleteev <contact@popleteev.com>
2012-01-26 01:23:20 +0100

    Fixed bus grid item sizes, verified their correct display on QVGA, HVGA, WVGA800 and XGA screens.

--- src/com/anmipo/android/trentobus/activities/BusGridAdapter.java
+++ src/com/anmipo/android/trentobus/activities/BusGridAdapter.java
@@ -3,19 +3,15 @@ package com.anmipo.android.trentobus.activities;
 import java.util.List;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
 
 import com.anmipo.android.trentobus.R;
 import com.anmipo.android.trentobus.db.BusInfo;
 
 public class BusGridAdapter extends ArrayAdapter<BusInfo> {
-	private static final int ITEM_VERTICAL_PADDING = 20;
     private List<BusInfo> buses;
 	
 	public BusGridAdapter(Context context, List<BusInfo> buses) {
@@ -40,7 +36,7 @@ public class BusGridAdapter extends ArrayAdapter<BusInfo> {
 			view = new ImageView(getContext());
 		}
 		BusInfo bus = buses.get(position);
-		view.setBackgroundDrawable(bus.getDrawable());
+		view.setImageDrawable(bus.getDrawable());
 		view.setId(position);
 		return view;
 	}
