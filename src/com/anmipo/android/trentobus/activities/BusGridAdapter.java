69be68bbfe449a100dff4d3c563f1f30355d8151
Andrei Popleteev <contact@popleteev.com>
2012-01-26 01:33:09 +0100

    Removed obsolete layout file and references to it.

--- src/com/anmipo/android/trentobus/activities/BusGridAdapter.java
+++ src/com/anmipo/android/trentobus/activities/BusGridAdapter.java
@@ -5,17 +5,17 @@ import java.util.List;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
-import com.anmipo.android.trentobus.R;
 import com.anmipo.android.trentobus.db.BusInfo;
 
-public class BusGridAdapter extends ArrayAdapter<BusInfo> {
+public class BusGridAdapter extends BaseAdapter {
     private List<BusInfo> buses;
-	
+	private Context context;
 	public BusGridAdapter(Context context, List<BusInfo> buses) {
-		super(context, R.layout.item_bus_number);
+		super();
+		this.context = context; 
 		this.buses = buses;
 	}
 	
@@ -23,21 +23,23 @@ public class BusGridAdapter extends ArrayAdapter<BusInfo> {
 	public int getCount() {
 		return buses.size();
 	}
-	
 	@Override
 	public BusInfo getItem(int position) {
 		return buses.get(position);
+	}
+	@Override
+	public long getItemId(int position) {
+		return position;
 	}
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ImageView view = (ImageView) convertView;
 		if (convertView == null) {
-			view = new ImageView(getContext());
+			view = new ImageView(context);
 		}
 		BusInfo bus = buses.get(position);
 		view.setImageDrawable(bus.getDrawable());
-		view.setId(position);
 		return view;
 	}
 }
